e11b2377bafc1e3ee36fb7fbba8f89a71e859555
salsabilaathika-97 <salsathikar@gmail.com>
2023-05-15 21:17:58 +0800

    create navbar component for dashboard page

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,51 @@
+import react from "react";
+import { useState } from "react";
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <div>
+            <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-slate-500 mb-3">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <a
+                            className="text-lg font-bold font-sans leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                            href="#pablo"
+                        >
+                            Practice 1 - Listening Test
+                        </a>
+                        <button
+                            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <i>=</i>
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center" +
+                            (navbarOpen ? " flex" : " hidden")
+                        }
+                        id="example-navbar-danger"
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="nav-item">
+                                <button class="bg-white text-slate-500 font-semibold py-2 px-4 mx-4 border border-slate-500">
+                                    Instruction
+                                </button>
+                            </li>
+                            <li className="nav-item">
+                                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
+                                    (x) Exit
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import react from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default Dashboard;
