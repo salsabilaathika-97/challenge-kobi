93f7ada7f990b3bc43d538ca0c632ff969294bf3
salsabilaathika-97 <salsathikar@gmail.com>
2023-05-16 15:50:44 +0800

    adjust audio bar and add icons

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useRef } from "react";
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
 import listen from "../../assets/ListeningTest.m4a"
@@ -50,6 +50,7 @@ const AudioPlayer = () => {
             setIsPlaying(true);
         }
     }
+
     return (
         <div class="flex flex-row w-full bg-slate-200 p-2">
             {
@@ -73,17 +74,27 @@ const AudioPlayer = () => {
                 <p class="mx-2">/</p>
                 <p class="ml-4">{time.min}:{time.sec}</p>
             </div>
-            <input
-                type="range"
-                min="0"
-                max={duration / 1000}
-                default="0"
-                value={seconds}
-                class="bg-slate-500 w-full"
-                onChange={(e) => {
-                    sound.seek([e.target.value])
-                }}
-            />
+            <div class="flex flex-row p-3 w-4/5">
+                <input
+                    type="range"
+                    min="0"
+                    max={duration / 1000}
+                    default="0"
+                    value={seconds}
+                    class="bg-slate-500 w-full"
+                    onChange={(e) => {
+                        sound.seek([e.target.value])
+                    }}
+                />
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 mx-2 my-4">
+                <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM18.584 5.106a.75.75 0 011.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 11-1.06-1.06 8.25 8.25 0 000-11.668.75.75 0 010-1.06z" />
+                <path d="M15.932 7.757a.75.75 0 011.061 0 6 6 0 010 8.486.75.75 0 01-1.06-1.061 4.5 4.5 0 000-6.364.75.75 0 010-1.06z" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-2 my-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+            </svg>
+
         </div>
     )
 }
