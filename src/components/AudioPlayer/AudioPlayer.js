9f3734791c51c5674ef3e4b9f30f6601bbdae681
salsabilaathika-97 <salsathikar@gmail.com>
2023-05-16 14:23:56 +0800

    change icon button in audio player

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -2,9 +2,6 @@ import react from "react";
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
 import listen from "../../assets/ListeningTest.m4a"
-import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
-import { IconContext } from "react-icons/lib";
-
 
 const AudioPlayer = () => {
     const [isPlaying, setIsPlaying] = useState(false);
@@ -13,9 +10,9 @@ const AudioPlayer = () => {
         min: "",
         sec: "",
     })
-    const[time, setTime] = useState({
+    const [time, setTime] = useState({
         min: "",
-        sec:""
+        sec: ""
     })
     const [seconds, setSeconds] = useState();
 
@@ -54,27 +51,27 @@ const AudioPlayer = () => {
         }
     }
     return (
-        <div class = "flex flex-row w-full bg-slate-200 p-2">
+        <div class="flex flex-row w-full bg-slate-200 p-2">
             {
                 !isPlaying ? (
                     <button class="justify-center border-collapse bg-transparent" onClick={handlePlay}>
-                        <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
-                            <AiFillPlayCircle />
-                        </IconContext.Provider>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />
+                        </svg>
                     </button>
                 ) :
                     (
                         <button class="justify-center border-collapse bg-transparent" onClick={handlePlay}>
-                            <IconContext.Provider value={{ size: "3em", color: "#27AE60" }}>
-                                <AiFillPauseCircle />
-                            </IconContext.Provider>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                <path fill-rule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clip-rule="evenodd" />
+                            </svg>
                         </button>
                     )
             }
-            <div class = "flex flex-row p-3">
+            <div class="flex flex-row p-3">
                 <p>{currentTime.min}:{currentTime.sec}</p>
-                <p class = "mx-2">/</p>
-                <p class = "ml-4">{time.min}:{time.sec}</p>
+                <p class="mx-2">/</p>
+                <p class="ml-4">{time.min}:{time.sec}</p>
             </div>
             <input
                 type="range"
